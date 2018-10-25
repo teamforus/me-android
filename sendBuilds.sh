6ba4bbe9f04ac503dae6318ebd98048472745861
Russian Hucker <pp@initflow.com>
2018-10-25 11:41:44 +0300

    ___

--- sendBuilds.sh
+++ sendBuilds.sh
@@ -1,4 +1,2 @@
 #./gradlew assembleDebugProdServer
 #./gradlew assembleDebugTestServer
-
-#telegram.sh -t 524800786:AAGnUobg5xBAhMJn3jXm8sKc48djLIBzmes -c -236697194 -f release_notes.txt "Build comments"
