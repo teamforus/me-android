72c1b55e06ffed7bd598a9808846cfcadd10dfc9
Russian Hucker <pp@initflow.com>
2018-10-25 11:34:01 +0300

    fixing ci

--- sendBuilds.sh
+++ sendBuilds.sh
@@ -1,2 +1,4 @@
 #./gradlew assembleDebugProdServer
 #./gradlew assembleDebugTestServer
+
+#telegram.sh -t 524800786:AAGnUobg5xBAhMJn3jXm8sKc48djLIBzmes -c -236697194 -f release_notes.txt "Build comments"
