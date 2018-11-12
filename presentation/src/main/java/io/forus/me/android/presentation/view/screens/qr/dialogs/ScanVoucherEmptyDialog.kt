d9d29b3ea8706a588f9ea415094d7ff6f46a5407
Denis Korobov <royalruby@yandex.ru>
2018-11-12 18:16:38 +0300

    scan voucher errors

--- presentation/src/main/java/io/forus/me/android/presentation/view/screens/qr/dialogs/ScanVoucherEmptyDialog.kt
+++ presentation/src/main/java/io/forus/me/android/presentation/view/screens/qr/dialogs/ScanVoucherEmptyDialog.kt
@@ -5,12 +5,12 @@ import com.afollestad.materialdialogs.MaterialDialog
 import io.forus.me.android.presentation.R
 
 
-class ScanVoucherNotEligibleDialog(private val context: Context,
-                                   private val dismissListener: () -> Unit){
+class ScanVoucherEmptyDialog(private val context: Context,
+                             private val dismissListener: () -> Unit){
 
     private val dialog: MaterialDialog = MaterialDialog.Builder(context)
             .title(R.string.qr_popup_error)
-            .content(R.string.qr_voucher_access_denied)
+            .content(R.string.qr_voucher_empty)
             .icon(context.resources.getDrawable(R.drawable.ic_close))
             .positiveText(context.resources.getString(R.string.me_ok))
             .dismissListener { dismissListener.invoke() }
