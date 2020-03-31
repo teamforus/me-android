81426e946c59be45aae2bc3e1d04a540a7777d20
maestromaster <maestrovsmaster@gmail.com>
2020-03-31 19:36:36 +0300

    fix restore by email description

---
+++ presentation/src/main/java/io/forus/me/android/presentation/helpers/SharedPref.kt
@@ -0,0 +1,49 @@
+package io.forus.me.android.presentation.helpers
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.app.Activity
+
+
+object SharedPref {
+    private var mSharedPref: SharedPreferences? = null
+    val RESTORE_EMAIL = "RESTORE_EMAIL"
+
+
+    fun init(context: Context) {
+        if (mSharedPref == null)
+            mSharedPref = context.getSharedPreferences(context.packageName, Activity.MODE_PRIVATE)
+    }
+
+    fun read(key: String, defValue: String): String? {
+        return mSharedPref!!.getString(key, defValue)
+    }
+
+    fun write(key: String, value: String) {
+        val prefsEditor = mSharedPref!!.edit()
+        prefsEditor.putString(key, value)
+        prefsEditor.commit()
+    }
+
+    fun read(key: String, defValue: Boolean): Boolean {
+        return mSharedPref!!.getBoolean(key, defValue)
+    }
+
+    fun write(key: String, value: Boolean) {
+        val prefsEditor = mSharedPref!!.edit()
+        prefsEditor.putBoolean(key, value)
+        prefsEditor.commit()
+    }
+
+    fun read(key: String, defValue: Int): Int {
+        return mSharedPref!!.getInt(key, defValue)
+    }
+
+    fun write(key: String, value: Int?) {
+        val prefsEditor = mSharedPref!!.edit()
+        prefsEditor.putInt(key, value!!).commit()
+    }
+}
+
+
+
