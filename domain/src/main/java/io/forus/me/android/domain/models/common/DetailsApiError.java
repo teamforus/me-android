171273f0e028dc9ef6f2ae98a74e862d6bc69a89
maestromaster <maestrovsmaster@gmail.com>
2020-04-08 15:15:09 +0300

    issue267: https://github.com/teamforus/me/issues/267 Implement backend error messages on doing transactions #267
    fix: instead hardcode "Transaction failed" will be show message from server such as "hoeveelheid dient minimaal .02 te zijn."
    an backend error (http 422 403) handler  was added for transactions,
    
    also added error handler foq QR scanner:
     for  - qr_unexpected_error
          - qr_popup_error (ScanVoucherNotEligibleDialog)
          - qr_identity_expired

---
+++ domain/src/main/java/io/forus/me/android/domain/models/common/DetailsApiError.java
@@ -0,0 +1,63 @@
+package io.forus.me.android.domain.models.common;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Provide map of errors for 422 status codes
+ */
+public class DetailsApiError {
+
+    private String message;
+
+
+    private HashMap<String, String> errors;
+
+    public DetailsApiError(String message, JsonObject errorsJson) {
+        this.message = message;
+
+        errors = new HashMap<>();
+
+        Set<Map.Entry<String, JsonElement>> entrySet = errorsJson.entrySet();
+        for (Map.Entry<String, JsonElement> entry : entrySet) {
+            String key = entry.getKey();
+            String value = entry.getValue().getAsString();
+            errors.put(key, value);
+        }
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public HashMap<String, String> getErrors() {
+        return errors;
+    }
+
+    @Override
+    public String toString() {
+        return "DetailsApiError{" +
+                "message='" + message + '\'' +
+                ", errors=" + errors.toString() +
+                '}';
+    }
+
+    public String getErrorString() {
+        StringBuilder msg = new StringBuilder();
+        for (String key : errors.keySet()) {
+            String value = errors.get(key);
+            String err = key + ": " + value + "\n";
+            msg.append(err);
+
+        }
+        return msg.toString();
+    }
+}
