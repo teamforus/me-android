4918ef6d7dc5ba5cc1cc700f41d866ce03fae36f
maestromaster <maestrovsmaster@gmail.com>
2020-04-21 14:54:13 +0300

    cleanup code issue 343

--- presentation/src/main/java/io/forus/me/android/presentation/view/screens/qr/dialogs/ScanDemoTransactionDialog.kt
+++ presentation/src/main/java/io/forus/me/android/presentation/view/screens/qr/dialogs/ScanDemoTransactionDialog.kt
@@ -9,7 +9,7 @@ class ScanDemoTransactionDialog(private val context: Context,
                                 private val positiveCallback: () -> Unit){
 
     private val dialog: MaterialDialog = MaterialDialog.Builder(context)
-            .title("Success") //translate will be available after merge issue #377 //context.resources.getString(R.string.qr_popup_restore_identity_title))
+            .title(context.resources.getString(R.string.voucher_send_email_success))
             .content(context.resources.getString(R.string.voucher_demo_transaction_operation_completed))
             .positiveText(context.resources.getString(R.string.me_ok))
             .onPositive { dialog, which -> positiveCallback.invoke() }
