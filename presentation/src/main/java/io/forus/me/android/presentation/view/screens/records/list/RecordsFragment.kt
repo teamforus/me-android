5a0d7fa901c01a4ac40c6bf516703d3daf6df6cc
Vasily Horodetskyi <maestrovsmaster@gmail.com>
2020-07-29 14:03:34 +0300

    issue 438 records/archive tabs

--- presentation/src/main/java/io/forus/me/android/presentation/view/screens/records/list/RecordsFragment.kt
+++ presentation/src/main/java/io/forus/me/android/presentation/view/screens/records/list/RecordsFragment.kt
@@ -1,29 +1,22 @@
 package io.forus.me.android.presentation.view.screens.records.list
 
-import android.app.Activity
-import android.arch.lifecycle.MutableLiveData
 import android.content.Intent
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.support.v7.widget.LinearLayoutManager
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
-import io.forus.me.android.presentation.view.base.lr.LRViewState
 import io.forus.me.android.presentation.R
 import io.forus.me.android.presentation.internal.Injection
-import io.forus.me.android.presentation.navigation.Navigator
+import io.forus.me.android.presentation.view.base.lr.LRViewState
 import io.forus.me.android.presentation.view.fragment.ToolbarLRFragment
 import io.forus.me.android.presentation.view.screens.records.create_record.CreateRecordActivity
 import io.forus.me.android.presentation.view.screens.records.item.RecordDetailsActivity
-import kotlinx.android.synthetic.main.fragment_records_recycler.*
-import kotlinx.android.synthetic.main.toolbar_view.*
-import io.forus.me.android.presentation.view.screens.records.list.RecordsPresenter
 import io.reactivex.Observable
 import io.reactivex.subjects.PublishSubject
-import kotlinx.android.synthetic.main.toolbar_view.profile_button
-import kotlinx.android.synthetic.main.toolbar_view_records.*
+import kotlinx.android.synthetic.main.fragment_records_recycler.*
+import kotlinx.android.synthetic.main.toolbar_view.*
 
 
 /**
@@ -55,11 +48,8 @@ class RecordsFragment : ToolbarLRFragment<RecordsModel, RecordsView, RecordsPres
 
         fun newIntent(): RecordsFragment = RecordsFragment().also {
 
-
         }
     }
-
-    val title = MutableLiveData<String>()
 
 
     override val allowBack: Boolean
@@ -72,7 +62,7 @@ class RecordsFragment : ToolbarLRFragment<RecordsModel, RecordsView, RecordsPres
         get() = false
 
     override val toolbarTitle: String
-        get() = title.value!!
+        get() = getString(R.string.dashboard_records)
 
     private var recordCategoryId: Long = 0
     private var recordCategoryName: String = ""
@@ -83,9 +73,6 @@ class RecordsFragment : ToolbarLRFragment<RecordsModel, RecordsView, RecordsPres
 
     override fun loadRefreshPanel() = lr_panel
 
-    init {
-        title.value = ""
-    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         val view = inflater.inflate(R.layout.fragment_records_recycler, container, false)
@@ -100,15 +87,12 @@ class RecordsFragment : ToolbarLRFragment<RecordsModel, RecordsView, RecordsPres
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-       // setToolbarTitle(recordCategoryName)
-        title.value = getString(R.string.dashboard_records)
-
-
         adapter = RecordsAdapter()
         adapter.clickListener = { item ->
-            //navigator.navigateToRecordDetailsForResult(context !! , item, LAUNCH_SECOND_ACTIVITY)
-            val intentToLaunch = RecordDetailsActivity.getCallingIntent(context!!, item)
-            startActivityForResult(intentToLaunch,LAUNCH_SECOND_ACTIVITY)
+            if(isRecords) {
+                val intentToLaunch = RecordDetailsActivity.getCallingIntent(context!!, item)
+                startActivityForResult(intentToLaunch, LAUNCH_SECOND_ACTIVITY)
+            }
         }
         recycler.layoutManager = LinearLayoutManager(context)
         recycler.adapter = adapter
@@ -133,27 +117,44 @@ class RecordsFragment : ToolbarLRFragment<RecordsModel, RecordsView, RecordsPres
     override fun render(vs: LRViewState<RecordsModel>) {
         super.render(vs)
 
-        Log.d("forus","Items= ${vs.model.items.size}")
-        Log.d("forus","ARChives= ${vs.model.archives.size}")
 
         if(isRecords) {
-            title.postValue(getString(R.string.dashboard_records))
+            setToolbarTitle(getString(R.string.dashboard_records))
             adapter.records = vs.model.items
         }else{
-            title.postValue("Archive")
             adapter.records = vs.model.archives
         }
         adapter.notifyDataSetChanged()
 
-        archive_button.setOnClickListener {
-            isRecords = !isRecords
-            if(isRecords) {
-                records.onNext(0)
-            }else{
-                archives.onNext(0)
-            }
+
+
+        tab1.setOnClickListener {
+            tab1title.setTextColor(ContextCompat.getColor(context!!,R.color.colorAccent))
+            tab2title.setTextColor(ContextCompat.getColor(context!!,R.color.gray_subtitle))
+
+            tab1divider.setBackgroundColor(ContextCompat.getColor(context!!,R.color.colorAccent))
+            tab2divider.setBackgroundColor(ContextCompat.getColor(context!!,R.color.silver))
+            isRecords = true
+            records.onNext(0)
+
+            addRecordBt.show()
+        }
+
+        tab2.setOnClickListener {
+            tab1title.setTextColor(ContextCompat.getColor(context!!,R.color.gray_subtitle))
+            tab2title.setTextColor(ContextCompat.getColor(context!!,R.color.colorAccent))
+
+            tab1divider.setBackgroundColor(ContextCompat.getColor(context!!,R.color.silver))
+            tab2divider.setBackgroundColor(ContextCompat.getColor(context!!,R.color.colorAccent))
+            isRecords = false
+            archives.onNext(0)
+
+            addRecordBt.hide()
         }
     }
+
+
+
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
