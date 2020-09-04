9ef2e1a3ad991c8cf0ce3107af0045a92a5a0f43
Vasily Horodetskyi <maestrovsmaster@gmail.com>
2020-09-04 17:39:09 +0300

    fix payment dialog ui

--- presentation/src/main/java/io/forus/me/android/presentation/view/screens/vouchers/dialogs/ApplyActionTransactionDialog.kt
+++ presentation/src/main/java/io/forus/me/android/presentation/view/screens/vouchers/dialogs/ApplyActionTransactionDialog.kt
@@ -10,6 +10,7 @@ import io.forus.me.android.presentation.R
 class ApplyActionTransactionDialog(private val context: Activity, private val amount: String,
                                    private val positiveCallback: () -> Unit) {
 
+    var dialog: MaterialDialog? = null
 
     fun show() {
 
@@ -19,16 +20,28 @@ class ApplyActionTransactionDialog(private val context: Activity, private val am
         customLayout.findViewById<TextView>(R.id.amount).text = amount
         customLayout.findViewById<TextView>(R.id.bottom).text = context.getString(R.string.paid_at_till)
 
-        val dialog: MaterialDialog = MaterialDialog.Builder(context)
+        customLayout.findViewById<io.forus.me.android.presentation.view.component.buttons.ButtonWhite>(R.id.submit).setOnClickListener {
+            positiveCallback.invoke()
+            dismiss()
+        }
+        customLayout.findViewById<io.forus.me.android.presentation.view.component.buttons.ButtonWhite>(R.id.cancel).setOnClickListener {
+            dismiss()
+        }
+
+
+         dialog= MaterialDialog.Builder(context)
                 .customView(customLayout, false)
-                .positiveText(context.resources.getString(R.string.submit))
-                .negativeText(context.resources.getString(R.string.me_cancel))
-                .onPositive { dialog, which -> positiveCallback.invoke() }
+                //.positiveText(context.resources.getString(R.string.submit))
+                //.negativeText(context.resources.getString(R.string.me_cancel))
+                //.onPositive { dialog, which -> positiveCallback.invoke() }
                 .build()
-
-
-
-        dialog.show()
+        dialog!!.show()
 
     }
+
+    fun dismiss(){
+        dialog!!.dismiss()
+    }
+
+
 }
